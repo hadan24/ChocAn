a44fa762b4696212bc67cdcd15a4351a7ac15dc2
Anh Ho <anhho@ada.cs.pdx.edu>
2022-11-15 22:48:27 -0800

    implememet member.cpp model.cpp

---
+++ model.cpp
@@ -0,0 +1,32 @@
+#include "chocan.h"
+
+model::model()
+{
+
+}
+
+model::model(const model & copy):
+    first_name(copy.first_name), last_name(copy.last_name)
+    ,address(copy.address), city(copy.city)
+    ,state(copy.state),email(copy.email)
+    ,zipcode(0000)
+{}
+model::~model()
+{
+
+}
+
+void model::display() const
+{
+
+}
+
+void model::read_file(const string& file_name) const
+{
+
+}
+
+void model::update_info()
+{
+    
+}
