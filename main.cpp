9b03fe9a93074a65372939ba3bbc42c00ced0a95
RyanKneeya <85848055+RyanKneeya@users.noreply.github.com>
2022-11-18 22:10:38 -0800

    Adding more functionality to main

--- main.cpp
+++ main.cpp
@@ -10,9 +10,46 @@ int main()
     tempID = query_ID();
 
     if (tempID == 1) {
-        provider_menu();
+        do {
+            provider_menu();
+            cout << "Select a Function: ";
+            menu_input = query_menu();
+            switch(menu_input) {
+                case 's':
+                    cout << "Checking Member Status..." << endl;
+                    break;
+                case 'i':
+                    cout << "Searching ID..." << endl;
+                    break;
+                case 'c':
+                    cout << "Charging Member..." << endl;
+                    break;
+                case 'd':
+                    cout << "Requesting Provider Directory..." << endl;
+                    break;
+                case 'a':
+                    cout << "Adding Member..." << endl;
+                    break;
+                case 'r':
+                    cout << "Removing Member" << endl;
+                    break;
+                case 'u':
+                    cout << "Updating Member Information..." << endl;
+                    break;
+                case 'o':
+                    cout << "Requesting Provider Report" << endl;
+                    break;
+                case 'q':
+                    cout << "Quitting..." << endl;
+                    break;
+                default:
+                    cout << "Unknown Command..." << endl << endl;
+                    break;
+            }
+        } while (menu_input != 'q');
+
     }
-    else if (tempID = 2) {
+    else if (tempID == 2) {
         do {
             member_menu();
             cout << "Select a Function: ";
@@ -26,6 +63,42 @@ int main()
                     break;
                 case 'q':
                     cout << "Quitting..." << endl << endl;
+                    break;
+                default:
+                    cout << "Unknown Command..." << endl << endl;
+                    break;
+            }
+        } while (menu_input != 'q');
+    }
+    else if (tempID == 3) {
+        do {
+            manager_menu();
+            cout << "Select a Function: ";
+            menu_input = query_menu();
+            switch (menu_input) {
+                case 'p':
+                    cout << "Printing Weekly Report" << endl;
+                    break;
+                case 'a':
+                    cout << "Adding Member" << endl;
+                    break;
+                case 'd':
+                    cout << "Removing Member" << endl;
+                    break;
+                case 'k':
+                    cout << "Adding Provider..." << endl;
+                    break;
+                case 'r':
+                    cout << "Removing Provider..." << endl;
+                    break;
+                case 'u':
+                    cout << "Updating Member Information..." << endl;
+                    break;
+                case 's':
+                    cout << "Updating Provider Information..." << endl;
+                    break;
+                case 'q':
+                    cout << "Quitting..." << endl;
                     break;
                 default:
                     cout << "Unknown Command..." << endl << endl;
@@ -60,7 +133,7 @@ void member_menu()
     print_line_break();
     cout << "r: Request service report" << endl;
     print_line_break();
-    cout << "q: Quit" << endl;
+    cout << "q: Quit" << endl << endl;
     print_line_break();
 }
 
@@ -84,7 +157,29 @@ void provider_menu()
     print_line_break();
     cout << "o: Request Provider Report" << endl;
     print_line_break();
-    cout << "q: Quit" << endl;
+    cout << "q: Quit" << endl << endl;
+    print_line_break();
+}
+
+void manager_menu() 
+{
+    print_header();
+
+    cout << "p: Print Weekly Report" << endl;
+    print_line_break();
+    cout << "a: Add Member" << endl;
+    print_line_break();
+    cout << "d: Remove Member" << endl;
+    print_line_break();
+    cout << "k: Add Provider" << endl;
+    print_line_break();
+    cout << "r: Remove Provider" << endl;
+    print_line_break();
+    cout << "u: Update Member Information" << endl;
+    print_line_break();
+    cout << "s: Update Provider Information" << endl;
+    print_line_break();
+    cout << "q: Quit" << endl << endl;
     print_line_break();
 }
 
@@ -105,7 +200,8 @@ int query_ID()
 char query_menu() 
 {
     char tempInput;
-    cin >> tempInput;
+    cin.ignore(100, '\n');
+    tempInput = getchar();
     cout << endl;
     return tolower(tempInput);
 }
--- main.h
+++ main.h
@@ -9,6 +9,7 @@ void print_header();
 void print_welcome();
 void member_menu();
 void provider_menu();
+void manager_menu();
 void print_line_break();
 
 //Query Block
