f16ca607c9a0b96619755f311c5651cb24c123a3
Anh Ho <anhho@ada.cs.pdx.edu>
2022-11-29 23:02:26 -0800

    adding info for input() function in model class

--- model.cpp
+++ model.cpp
@@ -1,4 +1,11 @@
+/*
+    11.29.2022 @aho
+        working on input() for model
+        checking data input is valid or not!
+        email input valid ref: https://www.geeksforgeeks.org/check-if-given-email-address-is-valid-or-not-in-cpp/
+*/
 #include "chocan.h"
+#include <regex>
 
 model::model():first_name("none"), last_name("node"), address("none")
             ,city("none"), state("none"), zipcode(0000), email("none")
@@ -18,7 +25,45 @@ model::~model()
 }
 void model::input()
 {
+    bool check_email = false;
+    cout << "Please enter your infomation in ChocAn system!\n";
+    cout << "Enter your fist name: ";
+    getline(cin, first_name);
 
+    cout << "Enter your last name: ";
+    getline(cin, last_name);
+
+    cout << "Enter your home address: ";
+    getline(cin, address);
+
+    cout << "Enter your city: ";
+    getline(cin, city);
+
+    cout << "Enter your state: ";
+    getline(cin, state);
+
+    cout << "Enter your zipcode: ";
+    cin >> zipcode;
+    cin.ignore(1000, '\n');
+    cin.clear();
+
+    do {
+    cout << "Enter your email: ";
+    getline(cin, email);
+    check_email = valid_email(email);
+      if (!check_email) {
+        cout << "Your email "<< email << " is "
+             << "INVALID. Please enter another one following (Eg: email@pdx.edu)" << endl;
+    }
+    } while (!check_email);
+}
+
+bool model::valid_email(const string& check_email)
+{  
+    // Regular expression definition
+    const regex pattern(
+        "(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+    return regex_match(check_email, pattern);
 }
 
 void model::display() const
